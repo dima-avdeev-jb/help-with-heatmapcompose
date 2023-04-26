ace338dccdb92eb5b6d4d1bb869c12a330cd2e4a
dima.avdeev <dima.avdeev@jetbrains.com>
2023-04-26 11:23:29 +0400

    minimal

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,40 +1,37 @@
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.unit.IntSize
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.launch
-import ru.madin.neurooptima2.dataset.CSVReader
-import ru.madin.neurooptima2.dataset.SpectrogramLine
-import ru.madin.neurooptima2.plot.SpectrogramPlot
-import java.io.File
-import java.util.*
-
 
 @Composable
 @Preview
-fun App(dataState : MutableStateFlow<List<SpectrogramLine>>) {
-
-    val data by dataState.collectAsState()
-    MaterialTheme {
-        SpectrogramPlot(
-            xrange = IntRange(start = 0, endInclusive = data.size),
-            yrange = IntRange(0,15),
-            data = data
-        )
+fun App() {
+    Canvas(modifier = Modifier.fillMaxSize().background(Color.White)) {
+        val chartWidth = size.width
+        val chartHeight = size.height
+        var x = 0f
+//        val rectSize: Float = 50f // works fine
+        val rectSize: Float = 50.5f // Show grid
+        while (x < chartWidth) {
+            var y = 0f
+            while (y < chartHeight) {
+                drawRect(Color.Black, Offset(x, y), Size(rectSize, rectSize))
+                y += rectSize
+            }
+            x += rectSize
+        }
     }
 }
 
 fun main() = application {
-    val data = CSVReader.readCsvToSpectrogram(File("src/jvmMain/resources/data.csv").inputStream())
-    val dataState = MutableStateFlow<List<SpectrogramLine>>(data)
     Window(onCloseRequest = ::exitApplication) {
-        App(dataState)
+        App()
     }
 }
